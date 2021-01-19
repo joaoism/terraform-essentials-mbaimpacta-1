6f215db2a10bda586022086ca04602a894f345c9
Gaby Dias <gabydias@gmail.com>
2021-01-18 22:20:44 -0300

    Alterando o Projeto do GCP

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "google" {
   version = "3.5.0"
 
-  project = "starry-argon-292200"
+  project = "gaby-cloudautomation"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
